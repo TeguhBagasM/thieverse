52ee90cafe922a25b4e5b99e4eb9976912d8b3a6
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-24 06:07:50 +0700

    style: changes card article ui

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -13,7 +13,7 @@ interface PostItemProps {
 
 export function PostItem({ slug, title, description, date, tags }: PostItemProps) {
   return (
-    <article className="flex flex-col gap-4 p-6 bg-gray-100 dark:bg-slate-900 rounded-lg shadow-lg transition-shadow hover:shadow-xl border border-gray-300 dark:border-slate-700 mb-4">
+    <article className="flex flex-col gap-4 p-6 bg-gray-100 dark:bg-slate-950 rounded-lg shadow-lg transition-shadow hover:shadow-xl border border-gray-300 dark:border-slate-700 mb-4">
       <div>
         <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-100 relative group inline-block">
           <Link href={"/" + slug}>
@@ -42,7 +42,7 @@ export function PostItem({ slug, title, description, date, tags }: PostItemProps
           href={"/" + slug}
           className="inline-flex items-center justify-center px-5 py-2 text-sm font-semibold text-white bg-blue-600 rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-300"
         >
-          Read more →
+          Read more
         </Link>
       </div>
     </article>
