6f069f19d86ee6395bf6b6d028710ab271b7637d
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-23 12:00:54 +0700

    style: update page in blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -37,7 +37,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
-          <p className="text-xl text-muted-foreground">My ramblings on all things web dev.</p>
+          <p className="text-xl text-muted-foreground">My ramblings about everything and coding.</p>
         </div>
       </div>
       <div className="grid grid-cols-12 gap-3 mt-8">
