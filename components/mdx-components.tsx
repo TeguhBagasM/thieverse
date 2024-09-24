5c27bee4a9fe19c1aff018f889fc978dd6b05004
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-24 08:29:45 +0700

    style: changes mdx ui content

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -18,5 +18,20 @@ interface MdxProps {
 
 export function MDXContent({ code }: MdxProps) {
   const Component = useMDXComponent(code);
-  return <Component components={components} />;
+  return (
+    <div
+      className="
+      prose-blockquote:bg-blue-500 
+      prose-blockquote:border-blue-500 
+      prose-blockquote:bg-blue-600/20
+      prose-blockquote:p-2
+      prose-blockquote:px-6
+      prose-blockquote:not-italic
+      prose-blockquote:rounded-r-lg
+      prose-li:marker:text-blue-600
+    "
+    >
+      <Component components={components} />
+    </div>
+  );
 }
--- velite.config.ts
+++ velite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
   mdx: {
     rehypePlugins: [
       rehypeSlug,
-      [rehypePrettyCode, { theme: "github-dark" }],
+      [rehypePrettyCode, { theme: "one-dark-pro" }],
       [
         rehypeAutolinkHeadings,
         {
