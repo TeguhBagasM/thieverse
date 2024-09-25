4cf2e318de6069aa7d79c337e6322b024385f31d
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-25 11:39:40 +0700

    chore: update ui card

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -15,27 +15,35 @@ interface PostItemProps {
 
 export function PostItem({ slug, title, date, tags, img }: PostItemProps) {
   return (
-    <article className="flex flex-col bg-white dark:bg-slate-950 rounded-xl shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-xl border border-gray-200 dark:border-slate-700">
-      <Link href={"/" + slug}>
-        {img ? (
-          <Image
-            src={img}
-            alt={title}
-            className="aspect-video w-full object-cover cursor-pointer"
-            width={400}
-            height={200}
-          />
-        ) : (
-          <div className="aspect-video w-full bg-gray-200 flex items-center justify-center">
-            <span className="text-gray-400">No image available</span>
-          </div>
-        )}
-      </Link>
+    <article className="flex flex-col bg-white dark:bg-slate-950 rounded-xl shadow-lg overflow-hidden group border border-gray-300 dark:border-slate-700">
+      <div className="aspect-[4/3] w-full overflow-hidden">
+        <Link href={"/" + slug}>
+          {img ? (
+            <Image
+              src={img}
+              alt={title}
+              className="w-full h-full object-cover object-center transition-transform duration-300 ease-in-out group-hover:scale-105"
+              sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+              width={400}
+              height={300}
+            />
+          ) : (
+            <div className="aspect-video w-full bg-gray-200 flex items-center justify-center">
+              <span className="text-gray-400">No image available</span>
+            </div>
+          )}
+        </Link>
+      </div>
       <div className="p-4">
         <div className="flex gap-2 mb-2">{tags?.map((tag) => <Tag tag={tag} key={tag} />)}</div>
         <Link href={"/" + slug}>
-          <h3 className="text-xl font-semibold text-gray-900 dark:text-white line-clamp-2 hover:text-indigo-600 transition-colors">
-            {title}
+          <h3 className="font-bold capitalize text-lg xs:text-base sm:text-xl md:text-2xl text-light mt-2 sm:mt-4">
+            <span
+              className="bg-gradient-to-r from-blue-500 to-blue-500 bg-[length:0px_4px] dark:from-blue-500/50 dark:to-blue-500/50
+                group-hover:bg-[length:100%_4px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 pb-1"
+            >
+              {title}
+            </span>
           </h3>
         </Link>
         <p className="text-sm sm:text-base font-sm mt-2 flex items-center gap-1">
