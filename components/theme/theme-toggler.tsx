9deefc70d4db764972a718eb78ecbd947498d6d0
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-26 10:59:15 +0700

    fix: ESLint rule @typescript-eslint/ban-ts-comment

--- components/theme/theme-toggler.tsx
+++ components/theme/theme-toggler.tsx
@@ -22,10 +22,10 @@ export default function ThemeToggler() {
   };
 
   const toggleTheme = () => {
-    // @ts-expect-error
+    // @ts-expect-error: startViewTransition might not be supported in all browsers
     if (!document.startViewTransition) switchTheme();
 
-    // @ts-expect-error
+    // @ts-expect-error: startViewTransition might not be supported in all browsers
     document.startViewTransition(switchTheme);
   };
 
