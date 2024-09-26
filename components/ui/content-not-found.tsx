a27c2ee2b4cf89b401944df2b790f76da29774f5
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-26 12:05:27 +0700

    style: update content-not-found ui

--- components/ui/content-not-found.tsx
+++ components/ui/content-not-found.tsx
@@ -1,12 +1,17 @@
 import { Frown } from "lucide-react";
 import React from "react";
 
+// Ganti dengan path gambar yang sesuai
+const NOT_FOUND_IMAGE_URL = "/images/not-found.png";
+
 const ContentNotFound = ({ text }: { text: string }) => {
   return (
-    <li role="listitem" className="h-60  w-full flex items-center justify-center">
-      <div className="space-y-3  flex flex-col items-center">
-        <Frown />
-        <h2 className="text-lg">{text}</h2>
+    <li role="listitem" className="h-auto w-full flex items-center justify-center">
+      <div className="space-y-3 flex flex-col items-center">
+        <img src={NOT_FOUND_IMAGE_URL} alt="Not Found" className="h-60 w-60 object-cover" />
+        <h2 className="text-xl flex items-center text-blue-500">
+          {text} <Frown className="ml-1" />
+        </h2>
       </div>
     </li>
   );
