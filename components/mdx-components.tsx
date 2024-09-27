fa0072c98fc8387a54a07e0c73f34f2e4e03c643
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-27 09:56:25 +0700

    chore: update blog and add youtube link

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -12,11 +12,39 @@ const useMDXComponent = (code: string) => {
   return fn({ ...runtime }).default;
 };
 
+function YouTube({
+  id,
+  title,
+  width = "100%",
+  height = "400px",
+}: {
+  id: string;
+  title: string;
+  width?: string;
+  height?: string;
+}) {
+  const src = `https://www.youtube.com/embed/${id}`;
+  return (
+    <div className="youtube-container my-5">
+      <iframe
+        width={width}
+        height={height}
+        src={src}
+        title={title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
 const components = {
   Image,
   Callout,
   Tab,
   Tabs,
+  YouTube,
   Disclosure,
   pre: CopyCodeBlock,
   Command,
