e15d24d0af98c3436f4d917e42657534a539de4d
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-09-28 17:16:11 +0700

    chore: update some page ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { cn, sortPosts } from "@/lib/utils";
 import { posts } from "#site/content";
 import Link from "next/link";
 import { PostItem } from "@/components/post-item";
-import { Instagram } from "lucide-react";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Home() {
   const latestPosts = sortPosts(posts).slice(0, 6);
@@ -13,13 +13,13 @@ export default function Home() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-2 lg:py-16">
         <div className="container flex flex-col-reverse md:flex-row items-center gap-8">
           <div className="flex-1 text-center md:text-left ml-2">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-5xl font-black text-balance">
-              Welcome to Thieverse
+            <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-4xl font-black text-balance">
+              Selamat datang di Thieverse
             </h1>
             <p className="max-w-[42rem] mx-auto md:mx-0 text-muted-foreground sm:text-xl text-balance mt-4">
-              Thieverse is my little corner of the internet where I talk about tech, web development,
-              and whatever else I&lsquo;m into. Besides articles and how-tos, I also share stories
-              about my life, from my love of tech to my hobbies.
+              Di sini, saya senang berbagi apa yang saya pelajari tentang teknologi. Selain artikel dan
+              tutorial, saya juga membuka pintu untuk konsultasi coding. Punya pertanyaan atau ingin
+              berdiskusi tentang tugasmu? Ayo, kita obrolkan!
             </p>
             <div className="flex flex-col gap-4 justify-center sm:flex-row mt-6">
               <Link
@@ -29,10 +29,10 @@ export default function Home() {
                   "w-full sm:w-fit bg-blue-500 dark:bg-blue-400 text-white hover:bg-blue-700 dark:hover:bg-blue-500 font-semibold py-3 px-8 rounded-full shadow-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:scale-105"
                 )}
               >
-                View my blog
+                Lihat Semua Blog
               </Link>
               <Link
-                href={siteConfig.links.instagram}
+                href={siteConfig.links.whatsapp}
                 target="_blank"
                 rel="noreferrer"
                 className={cn(
@@ -40,7 +40,7 @@ export default function Home() {
                   "w-full sm:w-fit border-2 border-blue-500 dark:border-blue-400 text-blue-500 dark:text-blue-400 py-3 px-8 rounded-full hover:bg-blue-500 dark:hover:bg-blue-400 hover:text-white dark:hover:text-white hover:border-blue-500 dark:hover:border-blue-400 shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 flex items-center justify-center"
                 )}
               >
-                <Instagram className="mr-2" /> Follow Me
+                <FaWhatsapp className="mr-2" /> Chat via Whatsapp
               </Link>
             </div>
           </div>
@@ -56,7 +56,7 @@ export default function Home() {
 
       <section className="container max-w-6xl py-6 lg:py-10 mt-18">
         <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-5xl font-black text-center mb-8">
-          Latest Blogs
+          Blog Terbaru
         </h2>
         <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-6">
           {latestPosts.map(
